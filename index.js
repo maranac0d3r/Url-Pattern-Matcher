0ba81784f4c9e33f91f4a3e4a2ab2ec90a7db57f
maranac0d3r <maranac0d3r@gmail.com>
2019-05-09 18:50:58 +0530

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function matches(pattern,url){
+    if(!url || !pattern){
+        return false
+    }
+    var patternArr = pattern.split("/")
+    var urlArr = url.split("/")
+    if(urlArr.length!=patternArr.length){
+        return false;
+    }git
+    for(var i=0;i<patternArr.length;i+=1){
+        if((patternArr[i].indexOf(":")!=0 && patternArr[i] != urlArr[i] )){
+            return false;
+        }
+
+    }
+    return true;
+}
+
+module.export =  {
+    matches:matches
+}
